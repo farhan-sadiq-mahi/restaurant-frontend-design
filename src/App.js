05d2dcd5d35a137572158cbcb3c07c1fc0fc3b14
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-12-09 09:25:47 +0600

    completed

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import Navbar from './components/Navbar/Navbar';
+import './App.css';
+import Header from './container/Header/Header';
+import AboutUs from './container/AboutUs/AboutUs';
+import SpecialMenu from './container/Menu/SpecialMenu';
+import Chef from './container/Chef/Chef';
+import Intro from './container/Intro/Intro';
+import Laurels from './container/Laurels/Laurels';
+import Gallery from './container/Gallery/Gallery';
+import FindUs from './container/FindUs/FindUs.jsx';
+import Footer from './container/Footer/Footer';
+
+const App = () => (
+  <div>
+
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
+
+export default App;
